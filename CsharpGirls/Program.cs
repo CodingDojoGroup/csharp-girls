2cce615c6eec6780d929ab5358c9c2579e1261f2
adrianapvent <adriana.e.pena@gmail.com>
2023-02-15 16:23:04 -0600

    list

--- CsharpGirls/Program.cs
+++ CsharpGirls/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//List
+List<string> names = new List<string>();
+
+names.Add("Mar'Kebta");
+names.Add("Sierra");
+names.Add("Adriana");
+
+foreach (string name in names)
+{
+    Console.WriteLine($"The CSharp girls are " + name);
+}
